ab8edad32e5769bcfa4209431991b919224c3cb2
Joel Rahman <joel@flowmatters.com.au>
2013-10-25 20:02:13 +1100

    Helper for reflecting on a class member (to_s defaults to method signature)

--- rb/helpers.rb
+++ rb/helpers.rb
@@ -8,3 +8,12 @@ def nice_methods(obj,matching=/.*/)
 	end
 	nil
 end
+
+def get_info(obj,method_name,ruby_name=true)
+	c = obj.GetType
+	if ruby_name
+		c.get_member(IronRuby::Clr::Name.unmangle(method_name))
+	else
+		c.get_member(method_name)
+	end
+end
